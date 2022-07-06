6570008baa7c255325a1a545147337f44dfd5bcb
Taras Nester <tarasnester414@gmail.com>
2022-07-06 19:46:10 +0300

    change static root directory

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

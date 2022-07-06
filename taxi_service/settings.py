aa86d715e5404206f1dc5dfef679e440d8ab8ab6
Taras Nester <tarasnester414@gmail.com>
2022-07-06 19:53:38 +0300

    change settings

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://taxi-service-1.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "taxi-service-1.herokuapp.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
